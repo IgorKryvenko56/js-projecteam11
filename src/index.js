c4bd53bb61808a96a8b6523f0835e6dbc7656905
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-05-09 21:40:32 +0300

    JS-11 import shosen

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/modal';
 import './js/categories-list';
 // import './js/bestsellers-list';
 // import './js/bestsellers-list2';
-import './js/chosen-category';
+// import './js/chosen-category';
 import './js/book-profile';
 //import './js/async-fetch';
 import './js/scroll-to-top';
